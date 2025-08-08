9bd92388e77ed3f56b4951f98d4f0329d07c549d
Hyejeong <thswjd0609@gmail.com>
2025-08-08 10:44:58 +0900

    first commit

---
+++ src/main/java/com/hanaro/shop/HanaroApplication.java
@@ -0,0 +1,13 @@
+package com.hanaro.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanaroApplication.class, args);
+    }
+
+}
