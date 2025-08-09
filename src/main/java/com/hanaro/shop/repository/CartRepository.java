113dfa7f3b0434466addce933fb9192b998a38ff
Hyejeong <thswjd0609@gmail.com>
2025-08-09 23:09:00 +0900

    feat: 레포지토리 생성

---
+++ src/main/java/com/hanaro/shop/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.hanaro.shop.repository;
+
+import com.hanaro.shop.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
---
+++ src/main/java/com/hanaro/shop/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.hanaro.shop.repository;
+
+import com.hanaro.shop.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
